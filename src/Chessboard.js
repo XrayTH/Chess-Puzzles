fb595264abb052cf03a1204b1884f54214d1176e
XrayTH <143976649+XrayTH@users.noreply.github.com>
2023-10-03 22:49:43 -0500

    solucion invalid movs v1

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -1,31 +1,34 @@
 import React, { useState } from 'react';
 import Chessboard from 'chessboardjsx';
-import {Chess} from 'chess.js'; // Asegúrate de instalar 'chess.js'
+import { Chess } from 'chess.js'; // Asegúrate de instalar 'chess.js'
 
 const ChessGame = () => {
-  const [chess] = useState(new Chess());
-  const [fen, setFen] = useState(chess.fen());
+    const [chess] = useState(new Chess());
+    const [fen, setFen] = useState(chess.fen());
 
-  const handleMove = (move) => {
-    if (chess.move(move)) {
-      // Actualiza el estado del tablero después de un movimiento válido
-      setFen(chess.fen());
-      console.log(move)
-     
-    }
-    
-  };
+    const handleMove = (move) => {
+        try {
+            if (chess.move(move) !== null) {
+                // Actualiza el estado del tablero después de un movimiento válido
+                setFen(chess.fen(move));
+                console.log(move)
 
-  
+            }
+        } catch (error) {
+            alert("Movimiento invalido")
+        }
+    };
 
-  return (
-    <div>
-      <Chessboard
-        position={fen}
-        onDrop={(move) => handleMove({ from: move.sourceSquare, to: move.targetSquare })}
-      />
-    </div>
-  );
+
+
+    return (
+        <div>
+            <Chessboard
+                position={fen}
+                onDrop={(move) => handleMove({ from: move.sourceSquare, to: move.targetSquare })}
+            />
+        </div>
+    );
 };
 
 export default ChessGame;

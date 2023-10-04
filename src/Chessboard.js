08e06957698386ca2b7d73160005ef0406f4d9de
XrayTH <143976649+XrayTH@users.noreply.github.com>
2023-10-04 16:34:01 -0500

    puzzle v1

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -1,21 +1,33 @@
 import React, { useState } from 'react';
 import Chessboard from 'chessboardjsx';
-import { Chess } from 'chess.js'; // Asegúrate de instalar 'chess.js'
+import { Chess } from 'chess.js'; 
 
 const ChessGame = () => {
-    const [chess] = useState(new Chess());
+    let mov = '8/7p/5B2/7P/3p4/3Pk1K1/8/q1N5 w - - 0 1';
+
+    const [chess] = useState(new Chess(mov));
     const [fen, setFen] = useState(chess.fen());
 
     const handleMove = (move) => {
         try {
-            if (chess.move(move) !== null) {
-                // Actualiza el estado del tablero después de un movimiento válido
+            if (chess.move(move)) {
+
                 setFen(chess.fen(move));
+                console.log(chess.fen(move))
                 console.log(move)
+
+                if (chess.isCheckmate()) {
+
+                    alert("Buen Trabajo!")
+                }else{
+                    alert("Erroneo!")
+                    setFen(chess.fen(chess.undo()))
+                }
 
             }
         } catch (error) {
-            alert("Movimiento invalido")
+            // alert("Erroneo!")
+            
         }
     };
 

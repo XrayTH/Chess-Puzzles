523d88cebad4113ce5b47846715b212968f96d6e
XrayTH <143976649+XrayTH@users.noreply.github.com>
2023-10-03 19:59:19 -0500

    mov validos v2

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -4,15 +4,18 @@ import {Chess} from 'chess.js'; // Asegúrate de instalar 'chess.js'
 
 const ChessGame = () => {
   const [chess] = useState(new Chess());
+  const [fen, setFen] = useState(chess.fen());
 
   const handleMove = (move) => {
     if (chess.move(move)) {
       // Actualiza el estado del tablero después de un movimiento válido
       setFen(chess.fen());
+     
     }
+    
   };
 
-  const [fen, setFen] = useState(chess.fen());
+  
 
   return (
     <div>

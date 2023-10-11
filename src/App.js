710899fd8d1f6a6043d05c2f062ac8c640d9df9f
XrayTH <143976649+XrayTH@users.noreply.github.com>
2023-10-11 18:47:43 -0500

    Diseño v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Chess from "./Chessboard";
+import "./App.css"
 
 function App() {
   return (
     <div>
+      <header>Chess Puzzles</header>
+
+      <nav>
+        <ul>
+          <li>Inicio</li>
+          <li>Perfil</li>
+          <li>Puzzles</li>
+          <li>Ranking</li>
+        </ul>
+      </nav>
+
+    <article>
+
+      <aside>
+        <h1>Puzzles:</h1>
+        <ul>
+          <li>Nivel 1</li>
+        </ul>
+      </aside>
+
       <Chess/>
+    </article>
+
     </div>
   );
 }
--- src/Chessboard.js
+++ src/Chessboard.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
 import Chessboard from 'chessboardjsx';
 import { Chess } from 'chess.js'; 
+import './Chess.css'
 
 const ChessGame = () => {
-    let mov = '8/7p/5B2/7P/3p4/3Pk1K1/8/q1N5 w - - 0 1';
+
+    const waifu = [
+        {
+            id: 1,
+            comentario: "Bienvenido onii-chan, haz el jaque en un solo mocimiento para ganar. <3"
+        },{
+            id: 2,
+            comentario: "Esa no es la solucion... ¡Sigue intentando! :)"
+        },{
+            id: 3,
+            comentario: "¡Hmp! Ese movimiento no es valido >:c"
+        },{
+            id: 4,
+            comentario: "¡Lo lograste! Buen trabajo, senpai >u<"
+        }
+    ]
+
+    let mov = '8/7p/5B2/7P/3p4/3Pk1K1/8/q1N5 w - - 0 1'
 
     const [chess] = useState(new Chess(mov));
     const [fen, setFen] = useState(chess.fen());
@@ -20,13 +38,13 @@ const ChessGame = () => {
 
                     alert("Buen Trabajo!")
                 }else{
-                    alert("Erroneo!")
+                    alert("Sigue intentando...")
                     setFen(chess.fen(chess.undo()))
                 }
 
             }
         } catch (error) {
-            alert("Erroneo!") 
+            alert("Movimiento invalido!") 
             
         }
     };
@@ -34,11 +52,24 @@ const ChessGame = () => {
 
 
     return (
-        <div>
+        <div id="principal">
+            <div id="Chessboard">
             <Chessboard
                 position={fen}
-                onDrop={(move) => handleMove({ from: move.sourceSquare, to: move.targetSquare })}
+                onDrop={(move) => handleMove({ from: move.sourceSquare, to: move.targetSquare, promotion: 'q'})}
+                width={500}
+                
             />
+            </div>
+
+            <div id="tools">
+                <div id="mainTools">
+                    <div id="comment">Dialogo...</div>
+                    <div id="time">0:00</div>
+                </div>
+                <div id="kwaii">Personaje :3</div>
+            </div>
+
         </div>
     );
 };

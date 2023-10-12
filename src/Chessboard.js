2f580a20616511c63084238cbbc9c2857f48fd38
XrayTH <143976649+XrayTH@users.noreply.github.com>
2023-10-11 19:02:28 -0500

    Diseño v1.1

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -7,21 +7,22 @@ const ChessGame = () => {
 
     const waifu = [
         {
-            id: 1,
-            comentario: "Bienvenido onii-chan, haz el jaque en un solo mocimiento para ganar. <3"
+            comentario: "Bienvenido onii-chan, haz el jaque en un solo movimiento para ganar. <3"
         },{
-            id: 2,
             comentario: "Esa no es la solucion... ¡Sigue intentando! :)"
         },{
-            id: 3,
             comentario: "¡Hmp! Ese movimiento no es valido >:c"
         },{
-            id: 4,
             comentario: "¡Lo lograste! Buen trabajo, senpai >u<"
+        },{
+            comentario: "Has perdido senpai... QwQ"
         }
     ]
 
     let mov = '8/7p/5B2/7P/3p4/3Pk1K1/8/q1N5 w - - 0 1'
+    const [com, setCom] = useState(waifu[0].comentario)
+    const [semueve, setSemueve] = useState(true)
+
 
     const [chess] = useState(new Chess(mov));
     const [fen, setFen] = useState(chess.fen());
@@ -32,19 +33,23 @@ const ChessGame = () => {
 
                 setFen(chess.fen(move));
                 console.log(chess.fen(move))
-                console.log(move)
+                
 
                 if (chess.isCheckmate()) {
 
-                    alert("Buen Trabajo!")
+                    //alert("Buen Trabajo!")
+                    setCom(waifu[3].comentario)
+                    setSemueve(false)
                 }else{
-                    alert("Sigue intentando...")
+                    //alert("Sigue intentando...")
+                    setCom(waifu[1].comentario)
                     setFen(chess.fen(chess.undo()))
                 }
 
             }
         } catch (error) {
-            alert("Movimiento invalido!") 
+            //alert("Movimiento invalido!")
+            setCom(waifu[2].comentario)
             
         }
     };
@@ -57,6 +62,7 @@ const ChessGame = () => {
             <Chessboard
                 position={fen}
                 onDrop={(move) => handleMove({ from: move.sourceSquare, to: move.targetSquare, promotion: 'q'})}
+                draggable ={semueve}
                 width={500}
                 
             />
@@ -64,7 +70,7 @@ const ChessGame = () => {
 
             <div id="tools">
                 <div id="mainTools">
-                    <div id="comment">Dialogo...</div>
+                    <div id="comment">{com}</div>
                     <div id="time">0:00</div>
                 </div>
                 <div id="kwaii">Personaje :3</div>
